d1c140cfd6e2b7863586454b893d9e61dbb9f5ca
Dhruv Kalaria <dhruvkalaria@gmail.com>
2016-11-30 00:56:18 -0800

    Slack & Wit.ai integration

--- bin/run.js
+++ bin/run.js
@@ -3,11 +3,11 @@
 const service = require('../server/service');
 const http = require('http');
 const slackClient = require('../server/slackClient');
-const witToken = '';
+const witToken = 'Q2OOSB7ISPLSRRDXTBGBXJVLXLDHMST6';
 const witClient = require('../server/witClient')(witToken);
 const server = http.createServer(service);
 
-const slackToken = '';
+const slackToken = 'xoxb-109290369106-Xrg9nYj94g72EyS0wIHNCJBb';
 const slackLogLevel = 'verbose';
 
 const rtm = slackClient.init(slackToken,slackLogLevel, witClient);
